21edd2bf1f359c37b0ad24d05d22bdd356005a8d
jairo <3519210003@uth.edu.mx>
2023-03-02 16:41:31 -0600

    mi aporte

--- demo.py
+++ demo.py
@@ -3,14 +3,14 @@ import colorsys
 t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor("black")
-t.speed(0)
+t.speed(-1)
 n = 36
 h = 0
-for i in range(460):
+for i in range(10):
 	c = colorsys.hsv_to_rgb(h,1,0.9)
 	h += 1/n
 	t.color(c)
-	t.left(145)
-	for j in range(5):
+	t.left(10)
+	for j in range(100):
 		t.forward(300)
 		t.left(150)
